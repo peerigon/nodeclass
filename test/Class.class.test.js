09775fb02bf5d7ef860903536ce8029900cc2e10
siR <siR@Pecorino>
2012-01-14 19:39:45 +0100

    - the constructor function has been extracted out of the class function into a separate file. This will improve readability and stress the importance of the construction function on performance. - tests in progress

--- test/Class.class.test.js
+++ test/Class.class.test.js
@@ -1,80 +1,102 @@
-var expect = require('expect.js'),
-    fs = require('fs'),
-    Class = require('../lib/Class.class');
+"use strict";
+
+var expect = require("expect.js"),
+    Class = require("../lib/Class.class"),
+    TestClass,
+    myTestClass;
+
+describe("Class", function () {
+    it("should return a function", function () {
+        TestClass = new Class({});
+        expect(TestClass).to.be.a("function");
+    });
+});
+
+describe("TestClassWithConstructor", function () {
+   it("should have a called constructor", function () {
+       var hasBeenCalled = false;
+
+       TestClass = new Class({
+           init: function () {
+               hasBeenCalled = true;
+           }
+       });
+
+       expect(hasBeenCalled).to.be("true");
+   })
+});
 
 
-
-/*
-var TestClass = new Class({
-            Extends: SuperClass,
-            init: function (param1, param2) {
-                console.log(param1, param2);
-            },
-            _someProperty: "a",
-            setSomeProperty: function (value) {
-                this._someProperty = value;
-            },
-            getSomeProperty: function () {
-                return this._someProperty;
-            }
-        });
- */
-
-
-
-
-
-
-
-
-
-describe('TestClass', function () {
+describe("TestClass", function () {
 
     var TestClass;
 
-    before(function(){
+    /*before(function () {
         TestClass = new Class({
-            someProperty : 'propertyValue',
-            _constructorCalled : false,
-            _privateProperty : 'privateValue',
+            someProperty : "propertyValue",
+            __constructorCalled : false,
+            __privateProperty : "privateValue",
             init: function (param1) {
+
                 //mark class as instantiated
-                console.log('init called!');
+                console.log("init called!");
                 this._constructorCalled = true;
+
             },
             getParam1: function (value) {
-                return(this.someProperty);
+
+                return this.someProperty;
+
             },
-            setParam1: function(newValue){
-                return this.someProperty = newValue;
+            setParam1: function (newValue) {
+
+                this.someProperty = newValue;
+
             },
-            doesExist : function(){
+            doesExist : function () {
                 return this._constructorCalled;
             }
         });
+    });*/
+
+    it("should return a function", function () {
+
+        TestClass = new Class({});
+
+        expect(TestClass).to.be.a("function");
     });
 
-    it('should be an object after creation', function () {
+    it("should be an object after creation", function () {
 
-        var myTestClass = TestClass('param1Value');
-        expect(myTestClass).not.to.be.a('undefined');
-        //expect(myTestClass.doesExist()).to.be(true);
+        var TestClass = new Class({}),
+            myTestClass = new TestClass();
+
+        expect(myTestClass).not.to.be("undefined");
     });
 
-    it('should have the defined method', function () {
-        var myTestClass = new TestClass('param1Value');
-        expect(myTestClass.getParam1).to.be.a('function');
+    it("should have the defined method", function () {
+
+        var myTestClass = new TestClass("param1Value");
+
+        expect(myTestClass.getParam1).to.be.a("function");
+
     });
 
-    it('should return properties via defined methods', function(){
-        var myTestClass = new TestClass('param1Value');
-        expect(myTestClass.getParam1()).to.equal('propertyValue');
+    it("should return properties via defined methods", function () {
+
+        var myTestClass = new TestClass("param1Value");
+
+        expect(myTestClass.getParam1()).to.equal("propertyValue");
+
     });
 
-    it('properties should be settable via setter', function(){
-          var myTestClass = new TestClass('constructPropertyValue');
+    it("properties should be settable via setter", function () {
+
+          var myTestClass = new TestClass("constructPropertyValue");
+
           myTestClass.setParam1("newValueHere");
-          expect(myTestClass.getParam1()).to.equal('newValueHere');
+          expect(myTestClass.getParam1()).to.equal("newValueHere");
+
     });
 
-});
+});*/
